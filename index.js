225cff619872a87363117a441d145309bccd0ef2
anthonymansur <amansur99@gmail.com>
2019-10-30 22:27:56 -0700

    updating increment

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html');
 });
 
-let count = 0;
+let users = new Array(100);
+const getUser = () => {
+    for (let i = 0; i < 100; i++) {
+        if (users[i] === 0) {
+            users[i] = 1;
+            return i;
+        }
+    }
+};
+const removeUser = (num) => {
+    users[i] = 0;
+};
 io.on('connection', socket => {
-    let user = ++count;
+    let user = getUser();
     io.emit('chat message', `user${user} connected`);
     socket.on('disconnect', () => {
+        removeUser(user);
         io.emit('chat message', `user${user} disconnected`);
     });
     socket.on('chat message', (msg) => {
